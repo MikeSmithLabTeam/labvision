25222c96f8d12420a51c8397d949f54eb5c991c0
mikesmithlab <mikesmithlab@gmail.com>
2024-08-22 17:37:16 +0100

    pyproject.toml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Mike Smith, James Downs'
 author = 'Mike Smith, James Downs'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0'
+release = '1.0.0'
 master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
